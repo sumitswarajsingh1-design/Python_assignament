c8c1d4019f12d5e4e971a2e514e3c85f8117b57d
Sumit Raj <sumitswarajsingh1@gmail.com>
2026-01-04 20:31:28 +0530

    Add files via upload

---
+++ task-1.py
@@ -0,0 +1,15 @@
+#Task 1: Perform Basic Mathematical Operations
+num1=int(input("enter the first number:"))
+num2=int(input("enrer the second number"))
+
+add=num1+num2
+sub=num1-num2
+multiply=num1*num2
+divison=num1/num2
+      
+print("the sum of",num1,'and',num2, "is:",add)
+print("the diffrens of",num1,"and",num2,"is:",sub)
+print("the multiplication of",num1,"and",num2,"is:", multiply)
+print("the divison of ",num1,"and",num2,"is:",divison)
+
+
---
+++ task.2.py
@@ -0,0 +1,8 @@
+#Create a Personalized Greeting
+n1=input("enter your first name:")
+n2=input("enter your last name:")
+
+name=n1+""+n2
+
+
+print("hello",name,"welcome to my python program.")
